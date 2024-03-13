f2a560bd3070b152d615f8b3f0129810028d2575
muskanadwani <89391901+muskanadwani@users.noreply.github.com>
2024-03-13 17:07:55 +0530

    first commit

---
+++ Connectify/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var router = express.Router();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const multer = require('multer');
+const hemlet = require('hemlet');
+const morgan = require('morgan');
+const path = require('path');
+import { fileURLToPath } from 'url';
+
+
+
+// Configurations
+
+const filename = fileURLToPath(import.meta, url);
+module.exports = router;
